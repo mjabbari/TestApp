e92a9f5bdc43eaaee7ad26fe7357c18784457251
Mohammad <m.jabbari123@gmail.com>
2017-09-27 15:09:32 +0400

    First Version

---
+++ C_Global.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UIKit;
+
+namespace ColorTap
+{
+	public class C_Global
+	{
+		public static UIColor bgColor = UIColor.FromRGB(94, 54, 177);// UIColor.FromRGB(55, 71, 79);
+		public static UIColor red = UIColor.FromRGB(255,110, 64);
+		public static UIColor yellow = UIColor.FromRGB(251, 192,42);
+		public static UIColor orange = UIColor.FromRGB(152, 200, 116);
+		public static UIColor btnBg = UIColor.FromRGB(69, 90, 100);
+
+		public static nfloat speedBubble = 2.5f;
+		public static nfloat speedInc = 2.5f;
+
+
+		public static nfloat scrWidth;
+		public static nfloat scrHeight;
+		public static nfloat scaling;
+
+
+
+
+	
+	}
+}
+
